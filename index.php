f831e38cbd071c9e72fb2e8d6de31bd1f7b556c0
JuanWong <juanwong0211@gmail.com>
2023-09-27 13:45:55 -0600

    Basic Router

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require "./Router.php";
+
+$router = new Router();
+
+$router->get('/test', function () {
+    return "GET OK";
+});
+
+$router->post('/test', function () {
+    return "POST OK";
+});
+$router->put('/test', function () {
+    return "PUT OK";
+});
+$router->patch('/test', function () {
+    return "PATCH OK";
+}); 
+$router->delete('/test', function () {
+    return "DELETE OK";
+});
+
+try {
+    $action = $router->resolve();
+    print($action());
+} catch (HttpNotFoundException $e) {
+    print("Not found");
+    http_response_code(404);
+}
+
