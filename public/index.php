73978344c5d6a7508cd3917668acc6c007d95729
JuanWong <juanwong0211@gmail.com>
2023-09-28 12:04:45 -0600

    Add Request class

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@
 require_once "../vendor/autoload.php";
 
 use Jc\HttpNotFoundException;
+use Jc\Request;
 use Jc\Route;
 use Jc\Router;
+use Jc\Server;
 
 $router = new Router();
 
@@ -26,7 +28,7 @@ $router->delete('/test', function () {
 });
 
 try {
-    $route = $router->resolve($_SERVER["REQUEST_URI"], $_SERVER["REQUEST_METHOD"]);
+    $route = $router->resolve(new Request(new Server()));
     $action = $route->action();
     print($action());
 } catch (HttpNotFoundException $e) {
