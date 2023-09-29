21ba045a13a8611ef3f9b9b740ef81e3f01d8402
JuanWong <juanwong0211@gmail.com>
2023-09-29 09:18:31 -0600

    Add Response class

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Jc\Http\HttpNotFoundException;
 use Jc\Http\Request;
+use Jc\Http\Response;
 use Jc\Routing\Router;
 use Jc\Server\PhpNativeServer;
 
@@ -11,11 +12,15 @@ require_once "../vendor/autoload.php";
 
 $router = new Router();
 
-$router->get('/test', function () {
-    return "GET OK";
+$router->get('/test', function (Request $request) {
+    $response = new Response();
+    $response->setHeader("Content-Type", "application/json");
+    $response->setContent(json_encode(["message" => "GET OK"]));
+
+    return $response;
 });
 
-$router->post('/test', function () {
+$router->post('/test', function (Request $request) {
     return "POST OK";
 });
 $router->put('/test', function () {
@@ -28,11 +33,17 @@ $router->delete('/test', function () {
     return "DELETE OK";
 });
 
+    $server = new PhpNativeServer();
 try {
-    $route = $router->resolve(new Request(new PhpNativeServer()));
+    $request = new Request($server);
+    $route = $router->resolve($request);
     $action = $route->action();
-    print($action());
+    $response = $action($request);
+    $server->sendResponse($response);
 } catch (HttpNotFoundException $e) {
-    print("Not found");
-    http_response_code(404);
+    $response = new Response();
+    $response->setStatus("404");
+    $response->SetContent("Not Found");
+    $response->setHeader("Content-Type", "text/plain");
+    $server->sendResponse($response);
 }
--- src/Server/Server.php
+++ src/Server/Server.php
@@ -3,10 +3,12 @@
 namespace Jc\Server;
 
 use Jc\Http\HttpMethod;
+use Jc\Http\Response;
 
 interface Server {
     public function requestUri(): string; 
     public function requestMethod(): HttpMethod;
     public function postData(): array; 
     public function queryParams(): array;
+    public function sendResponse(Response $response);
 }
