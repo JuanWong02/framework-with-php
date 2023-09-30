a1fcc0e03738723ff1c3f6fa23dace7724747ef1
JuanWong <juanwong0211@gmail.com>
2023-09-30 13:35:51 -0600

    Pass variables to views

--- src/view/View.php
+++ src/view/View.php
@@ -3,5 +3,5 @@
 namespace Jc\View;
 
 interface View {
-    public function render(string $view): string;
+    public function render(string $view, array $params = [], string $layout = null): string;
 }
