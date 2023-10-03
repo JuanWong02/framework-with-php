5b4f2b5c99e5ab1a30d797d0e87037343ef94322
JuanWong <juanwong0211@gmail.com>
2023-10-03 09:24:54 -0600

    Rollback migrations

---
+++ database/migrations/2023_10_03_000001_create_companies_table.php
@@ -0,0 +1,14 @@
+<?php
+
+use Jc\Database\DB;
+use Jc\Database\Migrations\Migration;
+
+return new class() implements Migration {
+    public function up() {
+        DB::statement('CREATE TABLE companies (id INT AUTO_INCREMENT PRIMARY KEY)');
+    }
+
+    public function down() {
+        DB::statement('DROP TABLE companies');
+    }
+};
