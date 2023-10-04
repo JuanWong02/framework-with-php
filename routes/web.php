3a522e84ce8c4c9b1647ab917b3381dd2fc0d922
JuanWong <juanwong0211@gmail.com>
2023-10-04 09:56:38 -0600

    Auth routes refactor

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Controllers\Auth\RegisterController;
 use App\Models\User;
-use Jc\Crypto\Hasher;
-use Jc\Http\Request;
+use Jc\Auth\Auth;
 use Jc\Http\Response;
 use Jc\Routing\Route;
+
+Auth::routes();
 
 Route::get('/', function () {
     if (isGuest()) {
@@ -18,31 +18,3 @@ Route::get('/form', fn () => view("form"));
 Route::get('/user/{user}', fn (User $user) => json($user->toArray()));
 Route::get('/route/{param}', fn (int $param) => json(["param" => $param]));
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
-
-Route::get('/login', fn () => view('auth/login'));
-
-Route::post('/login', function (Request $request){
-    $data = $request->validate([
-        "email" => ["required", "email"],
-        "password" => "required",
-    ]);
-
-    $user = User::firstWhere('email', $data['email']);
-
-    if(is_null($user) || !app(Hasher::class)->verify($data["password"], $user->password)) {
-        return back()->withErrors([
-            'email' => ['email' => 'Credentials do not match']
-        ]);
-    }
-    $user->login();
-
-    return redirect('/');
-
-});
-
-Route::get('/logout', function (){
-    auth()->logout();
-    return redirect('/');
-});
--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -2,14 +2,29 @@
 
 namespace Jc\Auth;
 
+use App\Controllers\Auth\LoginController;
+use App\Controllers\Auth\RegisterController;
 use Jc\Auth\Authenticators\Authenticator;
+use Jc\Routing\Route;
 
-class Auth {
-    public static function user(): ?Authenticatable {
+class Auth
+{
+    public static function user(): ?Authenticatable
+    {
         return app(Authenticator::class)->resolve();
     }
 
-    public static function isGuest(): bool {
+    public static function isGuest(): bool
+    {
         return is_null(self::user());
+    }
+
+    public static function routes()
+    {
+        Route::get('/register', [RegisterController::class, 'create']);
+        Route::post('/register', [RegisterController::class, 'store']);
+        Route::get('/login', [LoginController::class, 'create']);
+        Route::post('/login', [LoginController::class, 'store']);
+        Route::get('/logout', [LoginController::class, 'destroy']);
     }
 }
